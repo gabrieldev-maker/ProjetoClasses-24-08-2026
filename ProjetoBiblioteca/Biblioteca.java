67b130e080f60982e8877ee1c40aaf16d257647e
Gabriel Oliveira | Dev Junior <gabrieloliveirarod2310@gmail.com>
2026-08-24 12:40:44 -0300

    Update Livro constructor calls in Biblioteca
    Removed the 'isAvailable' parameter from Livro constructor calls.

--- ProjetoBiblioteca/Biblioteca.java
+++ ProjetoBiblioteca/Biblioteca.java
@@ -5,9 +5,9 @@ public class Biblioteca {
     Livro[] livros = new Livro[3];
 
     public Biblioteca() {
-        livros[0] = new Livro("Livro 1", "Autor 1", 2020, true);
-        livros[1] = new Livro("Livro 2", "Autor 2", 2019, false);
-        livros[2] = new Livro("Livro 3", "Autor 3", 2021, true);
+        livros[0] = new Livro("Livro 1", "Autor 1", 2020);
+        livros[1] = new Livro("Livro 2", "Autor 2", 2019);
+        livros[2] = new Livro("Livro 3", "Autor 3", 2021);
     }
     void adicionarLivro(Livro livro) {
         for (int i = 0; i < livros.length; i++) {
